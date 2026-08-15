13f39842cb2320e27b8998e66d183345b93451d0
manivannancsmca <manivannancsmca@gmail.com>
2026-08-15 17:12:38 +0530

    first commit

---
+++ src/main/java/com/payment/strategy/PaymentStrategyPatternApplication.java
@@ -0,0 +1,13 @@
+package com.payment.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentStrategyPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentStrategyPatternApplication.class, args);
+	}
+
+}
