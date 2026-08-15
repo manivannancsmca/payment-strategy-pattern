03d89cee8ce1791d18bae6759bf64a50969c6c8f
manivannancsmca <manivannancsmca@gmail.com>
2026-08-15 17:26:35 +0530

    StripePaymentStrategy class added

---
+++ src/main/java/com/payment/strategy/strategy/StripePaymentStrategy.java
@@ -0,0 +1,34 @@
+package com.payment.strategy.strategy;
+
+import java.util.UUID;
+
+import com.payment.strategy.dto.PaymentRequest;
+import com.payment.strategy.dto.PaymentResponse;
+import com.payment.strategy.model.PaymentProvider;
+
+public class StripePaymentStrategy implements PaymentStrategy {
+
+    @Override
+    public PaymentProvider getProvider() {
+         return PaymentProvider.STRIPE;
+    }
+
+    @Override
+    public PaymentResponse processPayment(PaymentRequest request) {
+        System.out.println(
+                "Calling Stripe API..."
+        );
+
+        String paymentId =
+                "STRIPE-" + UUID.randomUUID();
+
+        return new PaymentResponse(
+                request.orderId(),
+                PaymentProvider.STRIPE,
+                paymentId,
+                "SUCCESS",
+                "Stripe payment successful"
+        );
+    }
+
+}
