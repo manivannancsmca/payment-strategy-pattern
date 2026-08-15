702bae528808df32ce13fcacbee5fbfd615c3a72
manivannancsmca <manivannancsmca@gmail.com>
2026-08-15 17:24:57 +0530

    RazorpayPaymentStrategy class added

---
+++ src/main/java/com/payment/strategy/strategy/RazorpayPaymentStrategy.java
@@ -0,0 +1,34 @@
+package com.payment.strategy.strategy;
+
+import java.util.UUID;
+
+import com.payment.strategy.dto.PaymentRequest;
+import com.payment.strategy.dto.PaymentResponse;
+import com.payment.strategy.model.PaymentProvider;
+
+public class RazorpayPaymentStrategy implements PaymentStrategy {
+
+    @Override
+    public PaymentProvider getProvider() {
+       return PaymentProvider.RAZORPAY;
+    }
+
+    @Override
+    public PaymentResponse processPayment(PaymentRequest request) {
+       System.out.println(
+                "Calling Razorpay API..."
+        );
+
+        String paymentId =
+                "RZP-" + UUID.randomUUID();
+
+        return new PaymentResponse(
+                request.orderId(),
+                PaymentProvider.RAZORPAY,
+                paymentId,
+                "SUCCESS",
+                "Razorpay payment successful"
+        );
+    }
+
+}
