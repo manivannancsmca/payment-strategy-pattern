057b2d619945cd14f789628d27a8ccf14d8814e0
manivannancsmca <manivannancsmca@gmail.com>
2026-08-15 17:27:42 +0530

    PaypalPaymentStrategy classed added

---
+++ src/main/java/com/payment/strategy/strategy/PaypalPaymentStrategy.java
@@ -0,0 +1,34 @@
+package com.payment.strategy.strategy;
+
+import java.util.UUID;
+
+import com.payment.strategy.dto.PaymentRequest;
+import com.payment.strategy.dto.PaymentResponse;
+import com.payment.strategy.model.PaymentProvider;
+
+public class PaypalPaymentStrategy implements PaymentStrategy {
+
+    @Override
+    public PaymentProvider getProvider() {
+        return PaymentProvider.PAYPAL;
+    }
+
+    @Override
+    public PaymentResponse processPayment(PaymentRequest request) {
+        System.out.println(
+                "Calling PayPal API..."
+        );
+
+        String paymentId =
+                "PAYPAL-" + UUID.randomUUID();
+
+        return new PaymentResponse(
+                request.orderId(),
+                PaymentProvider.PAYPAL,
+                paymentId,
+                "SUCCESS",
+                "PayPal payment successful"
+        );
+    }
+
+}
